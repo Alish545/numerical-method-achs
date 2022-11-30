37ac4b9f4559686caf4e663dfc6bf02b554a4b2e
Alish545 <alish55tuladhar@gmail.com>
2022-11-30 15:26:49 +0545

    Lab work

---
+++ Lab 3/lab3_qn,10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+
+float f(int x, int y){
+    return pow(x, y);
+}
+
+float doubleINt(int xl, int xu, int yl, int yu){
+    return (yu - yl) * (xu - xl) * (f(xl, yl) + f(xl, yu) + f(xu, yu) + f(xu, yl))/4.0;  //formula..
+}
+
+int main(){
+    int xl, xu, yl, yu;
+    printf("Enter x limits (xl, xu): ");
+    scanf("%d %d",&xl,&xu);
+    printf("Enter y limits (yl, yu): ");
+   	scanf("%d %d",&yl,&yu);
+
+    printf("I= %f",doubleINt(xl, xu, yl, yu));
+    return 0;
+}
